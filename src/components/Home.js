739261bad0845352f68f34452c1f4b1ef1a0a41f
smoothbrady <bradyobholz@gmail.com>
2022-10-26 23:00:09 -0500

    front edit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,19 @@
+import React from "react"
+import { Navigate } from "react-router-dom"
+
+
 const Home = (props) => {
-	// const { msgAlert, user } = props
-	console.log('props in home', props)
+	const { user } = props
+	if (user) {
+		return(
+			<Navigate to='/marvels' />
+		)
+	}
 
 	return (
 		<>
 			<h2>Home Page</h2>
+			<p>Sign up or Login in to see Please!</p>
 		</>
 	)
 }
